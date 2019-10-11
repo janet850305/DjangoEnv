c8bda729cc203bcc139b1dbdf9193c685280f2d9
chelcho0305 <janet850305@gmail.com>
2019-10-11 18:44:09 +0800

    second

---
+++ myapp/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+def sayhello(request):
+	return HttpResponse("Hello Django ")
+
+def hello2(request,username):
+	return HttpResponse("Hello"+username)
+def hello3(request,username):
+	now = datetime.now()
+	return render(request,"hello3.html",locals())
+# Create your views here.
